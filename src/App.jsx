d7accde05d96de354968d3c012f940f3a06c542e
fika4life <fistewie@gmail.com>
2023-03-12 09:52:12 +0100

    adds ability to create affirmations with toasty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import AddAffirmation from './pages/AddAffirmation';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/add" element={<AddAffirmation />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/add" element={<AddAffirmation />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/AddAffirmation.jsx
+++ src/pages/AddAffirmation.jsx
@@ -1,3 +1,81 @@
+import { useState } from 'react';
+import { supabase } from '../client';
+import { toast } from 'react-toastify';
+// import { useNavigate } from 'react-router-dom';
+
 export default function AddAffirmation() {
-  return <div>AddAffirmation</div>;
+  // let navigate = useNavigate();
+  const [text, setText] = useState('');
+  const [posts, setPosts] = useState({ text: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //handle submit
+    createAffirmation();
+  };
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const notifySuccess = () =>
+    toast('Affirmation added', {
+      position: 'top-center',
+      hideProgressBar: true,
+      autoClose: 3000,
+      type: 'success'
+    });
+
+  const notifyError = (error) =>
+    toast(error, {
+      position: 'top-center',
+      hideProgressBar: true,
+      autoClose: 3000,
+      type: 'error'
+    });
+
+  async function createAffirmation() {
+    try {
+      const { data, error, status } = await supabase
+        .from('affirmations')
+        .insert({ text: text })
+        .select();
+
+      if (error) {
+        console.log(status);
+        throw error;
+      }
+
+      if (data) {
+        setText('');
+        notifySuccess();
+      }
+    } catch (error) {
+      console.log(error.message);
+      notifyError(error.message);
+    }
+  }
+  return (
+    <>
+      <h2>Add new affirmation</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="text">Affirmation</label>
+          <input
+            type="text"
+            name="text"
+            onChange={handleInputChange}
+            value={text}
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
